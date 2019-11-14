583dc56ef3d5053d5b2efd16661f90f850f16b53
Morten Kromberg <mkrom@dyalog.com>
2019-11-14 10:20:46 +0100

    Updated .NET Demo

--- DotNetAssy/Rundemo.ps1
+++ DotNetAssy/Rundemo.ps1
@@ -1,6 +1,7 @@
 Add-Type -Path C:\devt\deliverapl\dotnetassy\bridge171-64_unicode.dll
 Add-Type -Path C:\devt\deliverapl\dotnetassy\dyalognet.dll
 [Reflection.Assembly]::LoadFile("c:\devt\deliverapl\dotnetassy\Paths.dll")
-$text = [Mortens.Paths]::Involute(5)
-Write-Output $text
-Write-Output "Done"
+Write-Output ""
+
+$test = [Mortens.Paths]::Involute(5)
+Write-Output $test

89570bb502803079f298af6b0d6d66ff67f77c8d
Morten Kromberg <mkrom@dyalog.com>
2022-09-06 23:05:41 +0200

    Switch WebService from JSONServer to Jarvis

--- WebService/start.ps1
+++ WebService/start.ps1
@@ -1,9 +1,9 @@
-# Start "dyalog/jsonserver" docker image to server "involute" web service
+# Start "dyalog/jarvis" docker image to server "involute" web service
 # See invoke.ps1 for an example call to the running service
 # 
-$vol = "C:\Devt\deliverapl\WebService:/code" # Mapp application code to /code in container
-$ride = "RIDE_INIT=SERVE:*:4502"             # Allows RIDE connections for debugging (remove for prod deployment)
+$vol = "C:\Devt\deliverapl\WebService:/app" # Map application folder to "/app"
+$ride = "RIDE_INIT=SERVE:*:4502"            # Allows RIDE connections for debugging (remove for prod deployment)
 # -p 4502:4502                   Forward RIDE debugging port
 # -p 8080:8080                   Forward Web Service port
 
-docker run -v $vol -e $ride -p "4502:4502" -p "8080:8080" dyalog/jsonserver
+docker run -v $vol -e $ride -p "4502:4502" -p "8080:8080" dyalog/jarvis
